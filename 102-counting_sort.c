22018dabc3bcb17f868941ec21d5bd917e2bcd95
Ayoub <boulaidayoub02@gmail.com>
2024-04-29 11:17:42 -0700

    SOrting Algorithms

--- 102-counting_sort.c
+++ 102-counting_sort.c
@@ -1,89 +1,50 @@
 #include "sort.h"
-#include <stdio.h>
 #include <stdlib.h>
 
 /**
- * _calloc - Allocates memory for an array, and initializes it with zero.
- * @nmemb: Number of elements.
- * @size: Size of each element.
- * Return: Pointer to the allocated memory, or NULL if allocation fails.
- */
-void *_calloc(unsigned int nmemb, unsigned int size)
-{
-    unsigned int i;
-    char *p;
-
-    if (nmemb == 0 || size == 0)
-        return (NULL);
-
-    p = malloc(nmemb * size);
-    if (p == NULL)
-        return (NULL);
-
-    for (i = 0; i < (nmemb * size); i++)
-        p[i] = '\0';
-
-    return (p);
-}
-
-/**
- * counting_sort - Sorts an array of integers using the Counting Sort algorithm.
- * @array: The array of integers to be sorted.
- * @size: The size of the array.
+ * counting_sort - sorts an array of integers in ascending order
+ * using the Counting sort algorithm
+ * @array: Array to be sorted
+ * @size: size of the array
+ *
+ * Return: void
  */
 void counting_sort(int *array, size_t size)
 {
-	int *counter, *tmp;
-	int index, maximum = 0;
-	size_t i;
+	int *count_arr, k;
+	size_t i, j, arr_size;
 
-	if (array == NULL || size < 2)
+	if (array == NULL || size <= 1)
+		return;
+	arr_size = array[0];
+	for (i = 0; array[i]; i++)
+	{
+		if (array[i] > (int)arr_size)
+			arr_size = array[i];
+	}
+
+	arr_size += 1;
+
+	count_arr = malloc(arr_size * sizeof(int *));
+	if (count_arr == NULL)
 		return;
 
-	/* Find maximum number in the array*/
-	for (i = 0; i < size; i++)
-	{
-		if (array[i] > maximum)
-			maximum = array[i];
-	}
-
-	counter = _calloc(maximum + 1, sizeof(int));
-	tmp = _calloc(size, sizeof(int));
-	if (counter == NULL || tmp == NULL)
-	{
-		free(counter);
-		free(tmp);
-		return;
-	}
+	for (i = 0; i < arr_size; i++)
+		count_arr[i] = 0;
 
 	for (i = 0; i < size; i++)
-	{
-		counter[array[i]]++;
-	}
+		count_arr[array[i]] += 1;
 
-	/* Get cumulative counts*/
-	for (index = 1; index <= maximum; index++)
-	{
-		counter[index] += counter[index - 1];
-	}
+	for (i = 0; i <= arr_size; i++)
+		count_arr[i] += count_arr[i - 1];
 
-	/* Sort the array based on counts*/
-	for (i = 0; i < size; i++)
-	{
-		tmp[counter[array[i]] - 1] = array[i];
-		counter[array[i]]--;
-	}
+	print_array(count_arr, arr_size);
 
-	/*Copy sorted elements back to original array*/
-	for (i = 0; i < size; i++)
-	{
-		array[i] = tmp[i];
-	}
-
-	/*Print the sorted array*/
-	print_array(array, size);
-
-	/*Free allocated memory*/
-	free(counter);
-	free(tmp);
+	for (i = 1, j = 0; i <= arr_size; i++)
+		if (count_arr[i] != count_arr[i - 1])
+		{
+			for (k = 0; k < count_arr[i] - count_arr[i - 1]; k++)
+				array[j++] = i;
+		}
+	free(count_arr);
 }
